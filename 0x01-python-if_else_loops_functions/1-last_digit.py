b19a942c5f9b540b6518d8cd4ef341887164b3f8
Matsadura <zidanditoo1@gmail.com>
2023-10-31 14:19:18 +0100

    rasi mrb3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_n = abs(number) % 10
+if number < 0:
+    last_n = last_n * -1
 string_1 = "Last digit of {} is {} and is greater than 5"
 string_2 = "Last digit of {} is {} and is less than 6 and not 0"
 if last_n > 5:
