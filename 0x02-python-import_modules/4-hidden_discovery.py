f3c05cd153b46ea923e620c7b0ea0cd060ed2228
Matsadura <zidanditoo1@gmail.com>
2023-11-02 14:26:32 +0100

    Remove code execution with __import__

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
 
-for i in range(len(dir(hidden_4))):
-    if dir(hidden_4)[i].startswith("__"):
-        continue
-    else:
-        print(dir(hidden_4)[i])
+if __name__ == "__main__":
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i].startswith("__"):
+            continue
+        else:
+            print(dir(hidden_4)[i])
