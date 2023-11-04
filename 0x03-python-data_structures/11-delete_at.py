67441356c80ccfef3f87c52b0110d9f59a8e0042
Matsadura <zidanditoo1@gmail.com>
2023-11-04 14:16:59 +0100

    test requirement check

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,10 @@
 def delete_at(my_list=[], idx=0):
     if idx >= len(my_list):
         return my_list
-    del(my_list[idx])
-    return my_list
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list += my_list[i]
+        else:
+            continue
+    return new_list
