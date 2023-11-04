5034866d4d4dfbe6a901b1a9900d66d75e24f084
Matsadura <zidanditoo1@gmail.com>
2023-11-04 13:46:17 +0100

    Fix empty sentence case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,8 @@
 def multiple_returns(sentence):
     if sentence == '':
         first = None
+        tup = len(sentence), first
+        return tup
 
     tup = len(sentence), sentence[0]
     return tup
