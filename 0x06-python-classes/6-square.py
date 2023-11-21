727ac5e77e26c104fa69875bc6c1d498045517a2
Matsadura <zidanditoo1@gmail.com>
2023-11-21 23:24:56 +0100

    add condition to not print line if p[1] > 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -67,11 +67,12 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for l in range(self.__position[1]):
+            for L in range(self.__position[1]):
                 print()
             for i in range(self.__size):
                 for k in range(self.__position[0]):
-                    print(" ", end="")
+                    if (self.__position[1] <= 0):
+                        print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
