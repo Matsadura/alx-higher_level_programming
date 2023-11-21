64fd6e2431b8f74e5b22d48ee4a7d8c1ea3201bc
Matsadura <zidanditoo1@gmail.com>
2023-11-21 22:42:14 +0100

    Add task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+class Square:
+    """
+    """
+    def __init__(self, size=0):
+        """
+            instantiation
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            getter
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+            setter
+        """
+        if type(value) is not int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif self.__size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+            Area of the square
+        """
+        return self.__size ** 2
