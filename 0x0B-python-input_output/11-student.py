d1dfdac1bc8317245f81803c7bca36df2bd32f11
Matsadura <zidanditoo1@gmail.com>
2023-12-05 22:04:01 +0100

    Loop instead of hard code

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -20,6 +20,8 @@ class Student:
 
     def reload_from_json(self, json):
         """ Replaces all attributes of the Student instance """
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        # self.first_name = json['first_name']
+        # self.last_name = json['last_name']
+        # self.age = json['age']
+        for key in json:
+            setattr(self, key, json[key])
