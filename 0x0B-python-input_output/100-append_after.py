42f43975be354982a34b469e36ccc2d15be0d306
Matsadura <zidanditoo1@gmail.com>
2023-12-05 22:21:28 +0100

    Fix permission to write

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -15,7 +15,7 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
 
-    with open(filename, 'a+', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         for line in lines:
             f.write(line)
             if search_string in line:
