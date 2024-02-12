3986af4c82a3ff6cfa4eb91024bdf59b630e3629
Matsadura <zidanditoo1@gmail.com>
2024-02-12 13:29:50 +0100

    [UPDATE] Documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,6 +9,17 @@ class Rectangle:
         A class that defines a Rectangle
     """
     def __init__(self, width=0, height=0):
+        """
+            An instance constructor for the class Rectangle
+
+            Args:
+                width (int, optional): the width of the rectangle
+                height (int, optional): the height of the rectanlge
+
+            Raises:
+                TypeError: if width or height are not an integer
+                ValueError: if width or height are < 0
+        """
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
@@ -22,14 +33,30 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+            A property getter of height
+        """
         return self.__height
 
     @property
     def width(self):
+        """
+            A property getter of width
+        """
         return self.__width
 
     @height.setter
     def height(self, value):
+        """
+            A property setter of height:
+
+            Args:
+                value (int): the new height value to set
+
+            Raises:
+                TypeError: if height is not an int
+                ValueError: if height < 0
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -38,6 +65,16 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """
+            A property setter of height
+
+            Args:
+                value (int): the new width value to set
+
+            Raises:
+                TypeError: if width is not an int
+                ValueError: if width < 0
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
