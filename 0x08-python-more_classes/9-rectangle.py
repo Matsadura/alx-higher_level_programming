218ee09f3ff021ab05008282a2c4e502e23753a0
Matsadura <zidanditoo1@gmail.com>
2024-02-12 17:41:14 +0100

    [UPDATE] switch height with width

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -23,16 +23,16 @@ class Rectangle:
                 TypeError: if width or height are not an integer
                 ValueError: if width or height are < 0
         """
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
