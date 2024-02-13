de01128297fdbc596cc65d31a404d485acab2212
Matsadura <zidanditoo1@gmail.com>
2024-02-13 12:16:53 +0100

    [TO REVISE/TO DO]

---
+++ 0x00-python-hello_world/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN
+#define MAIN
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int check_cycle(listint_t *list);
+int _putchar(char c);
+#endif /* MAIN */
---
+++ 0x01-python-if_else_loops_functions/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN
+#define MAIN
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+def islower(c):
+	def uppercase(str):
+		def print_last_digit(number):
+			def add(a, b):
+				def pow(a, b):
+					def fizzbuzz():
+						listint_t *insert_node(listint_t **head, int number);
+						int _putchar(char c);
+#endif /* MAIN */
