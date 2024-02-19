c39de630f3489b2cf6e557eed9f0d782c4d5d32b
Matsadura <zidanditoo1@gmail.com>
2024-02-19 12:52:16 +0100

    [ADD] Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    A module that contains BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+        A class
+
+        Methods:
+            Area: Not yet implemented
+    """
+    def area(self):
+        """
+            A method that has not been implemented yet
+        """
+        raise Exception("area() is not implemented")
