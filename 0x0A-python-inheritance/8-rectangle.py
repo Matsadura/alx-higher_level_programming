52e08ab896c75a57119f83dab5c7bf692f510232
Matsadura <zidanditoo1@gmail.com>
2024-02-19 16:33:34 +0100

    [UPDATE] Remove import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,14 +3,36 @@
     A module that contains a class Rectangle
 """
 
+class BaseGeometry:
+    """
+        A class
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+        Methods:
+            Area: Not yet implemented
+    """
+    def area(self):
+        """
+            A method that has not been implemented yet
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+            A method that validates value
+
+            Args:
+                name: A string
+                value: The value to validate
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """
         A class Rectangle that inherits from BaseGeometry
     """
-
     def __init__(self, width, height):
         """
             Constructor of instances
