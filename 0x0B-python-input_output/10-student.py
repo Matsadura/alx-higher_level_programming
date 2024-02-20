31e3e077971a325cf3be015db34011fec167c2a1
Matsadura <zidanditoo1@gmail.com>
2024-02-20 13:28:34 +0100

    [UPDATE] New approach again

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-"""
-    A module that contains the class Student
-"""
+"""10-student"""
 
 
-class Student():
-    """
-        A class that defines a Student by:
-        first_name
-        last_name
-        age
-    """
+class Student:
+    """ A class that defines a student """
     def __init__(self, first_name, last_name, age):
-        """
-        An attribute constructor
-        """
+        """ an instance of Student """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        A method that retrives a dictionary repr of a Student
-        """
-        rep = {}
-        if attrs:
-            for attr in attrs:
-                if hasattr(self, attr):
-                    rep[attr] = getattr(self, attr)
-            return rep
-        return vars(self)
+        """ Returns a dictionary representation of a Student """
+
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            return {attr: getattr(self, attr) for
+                    attr in attrs if hasattr(self, attr)}
+        return self.__dict__
