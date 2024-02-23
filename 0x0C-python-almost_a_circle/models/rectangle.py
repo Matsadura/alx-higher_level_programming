914e9a80ec3e3968788d59be1da065a0f546d17e
Matsadura <zidanditoo1@gmail.com>
2024-02-23 20:33:59 +0100

    [UPDATE] Task 4 ~ 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,9 +15,8 @@ class Rectangle(Base):
         """
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        elif width <= 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
-        self.__width = width
 
         if type(height) is not int:
             raise TypeError("height must be an integer")
@@ -37,6 +36,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = y
 
+        self.__width = width
         super().__init__(id)
 
     @property
@@ -45,6 +45,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -53,6 +57,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -61,6 +69,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -69,4 +81,32 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        print('\n' * self.__y + (
+              ' ' * self.__x + '#' * self.__width + '\n') * self.__height,
+              end="")
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
+
+    def update(self, *args):
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
