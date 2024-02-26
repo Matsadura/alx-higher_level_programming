dd363bb5f39f03f2b2df26aef712e4eee22248ac
Matsadura <zidanditoo1@gmail.com>
2024-02-26 09:57:22 +0100

    [ADD] Tasks 20 & 21

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,8 +3,10 @@
     A module for the class Base
 """
 import json
+import csv
 import os
-
+import turtle
+import random
 
 class Base:
     """
@@ -107,3 +109,97 @@ class Base:
             list_of_inst = [cls.create(**inst) for inst in dict_json]
             return list_of_inst
         return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+            A class method that serializes in CSV
+
+            Args:
+                list_objs (list): List of instances that inherits of Base
+        """
+        if list_objs:
+            list_of_dict = [obj.to_dictionary() for obj in list_objs]
+        with open(cls.__name__ + '.csv', 'w') as f:
+            if cls.__name__ == 'Rectangle':
+                names = ['id', 'width', 'height', 'x', 'y']
+            elif cls.__name__ == 'Square':
+                names = ['id', 'size', 'x', 'y']
+            writer = csv.DictWriter(f, fieldnames=names)
+
+            writer.writeheader()
+            for line in list_of_dict:
+                writer.writerow(line)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+            A class method that deserializes in CSV
+
+            Returns:
+                list_of_inst (list): List of instances
+        """
+
+        with open(cls.__name__ + '.csv', 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            list_objs = [row for row in reader]
+            json_str = cls.to_json_string(list_objs)
+            dict_json = cls.from_json_string(json_str)
+            new_dict = []
+            for mini_dict in dict_json:
+                for key, value in mini_dict.items():
+                    mini_dict[key] = int(value)
+                new_dict.append(mini_dict)
+            list_of_inst = [cls.create(**inst) for inst in new_dict]
+            return list_of_inst
+
+    @staticmethod
+    def draw(list_rectangle, list_square):
+        """
+            A static method that opens a window and draws
+            all the Rectangles and Squares
+
+            Args:
+                list_rectangle (list): The list of rectangles to draw
+                list_squares (list): The list of squares to draw
+        """
+        list_colors = ['red', 'blue', 'green', 'purple', 'brown', 'yellow',
+                'orange', 'cyan', 'violet']
+        drawing = turtle.Turtle()
+        for rect in list_rectangle:
+            drawing.color(random.choice(list_colors))
+            drawing.fillcolor(random.choice(list_colors))
+            drawing.forward(rect.x)
+            drawing.right(90)
+            drawing.forward(rect.y)
+            drawing.pd()
+            drawing.begin_fill()
+            drawing.fd(rect.width)
+            drawing.right(90)
+            drawing.fd(rect.height)
+            drawing.right(90)
+            drawing.fd(rect.width)
+            drawing.right(90)
+            drawing.fd(rect.height)
+            drawing.end_fill()
+            drawing.pu()
+
+        for sqr in list_square:
+            drawing.color(random.choice(list_colors))
+            drawing.fillcolor(random.choice(list_colors))
+            drawing.forward(sqr.x)
+            drawing.right(90)
+            drawing.forward(sqr.y)
+            drawing.pd()
+            drawing.begin_fill()
+            drawing.fd(sqr.size)
+            drawing.right(90)
+            drawing.fd(sqr.size)
+            drawing.right(90)
+            drawing.fd(sqr.size)
+            drawing.right(90)
+            drawing.fd(sqr.size)
+            drawing.end_fill()
+            drawing.pu()
+
+        turtle.done()
