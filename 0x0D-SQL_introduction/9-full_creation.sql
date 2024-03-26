8a5676314a7028dc72bca7874e3e817e57eee655
Matsadura <zidanditoo1@gmail.com>
2024-03-26 22:09:35 +0000

    [UPDATE] Add IF NOT EXISTS condition

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a second_table and insert more records into it
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score)
