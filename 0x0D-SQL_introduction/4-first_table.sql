e500d0dbd78ca8e64a0f87bb0f23d5ce294d56e3
Matsadura <zidanditoo1@gmail.com>
2024-03-26 21:18:20 +0000

    [UPDATE] Fix Typo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Creates a table first_table in the current database
 -- it contains id (INT), name(VARCHAR(256))
-CREATE TABLE IF NOT EXISTS first_name (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
