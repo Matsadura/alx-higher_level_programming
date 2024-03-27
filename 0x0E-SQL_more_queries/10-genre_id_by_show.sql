c06eb7ccc92f76ecbec361222f52668235d923b3
Matsadura <zidanditoo1@gmail.com>
2024-03-27 23:41:41 +0000

    [UPDATE] Change from JOIN to WHERE

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- Lists all shows contained in the imported database
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY title ASC, genre_id ASC;
