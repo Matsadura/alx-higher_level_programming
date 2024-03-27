a4ec9f365689e6beb98f931fb504ded65aad5ca8
Matsadura <zidanditoo1@gmail.com>
2024-03-27 23:35:37 +0000

    [UPDATE] add another ASC

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.titles, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genreS
 ON tv_shows.id = tv_show_genres.genre_id
-ORDER BY title, genre_id ASC;
+ORDER BY title ASC, genre_id ASC;
