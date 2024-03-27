456ee3616f73621cbbb46676d091df45f37fdfb7
Matsadura <zidanditoo1@gmail.com>
2024-03-27 23:38:53 +0000

    [UPDATE] Change genre_id to show_id

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Lists all shows contained in the imported database
-SELECT tv_shows.titles, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genreS
-ON tv_shows.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title ASC, genre_id ASC;
