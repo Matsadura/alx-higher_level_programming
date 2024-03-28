4bd859c08c5a2d23418ac948beb92a5b325ee90a
Matsadura <zidanditoo1@gmail.com>
2024-03-28 00:11:43 +0000

    [UPDATE] Add ORDER BY DESC

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,5 @@ COUNT(tv_genre.name) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_shows_genre
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
