faa37354679919b919924c095f82ad65856b00c8
Matsadura <zidanditoo1@gmail.com>
2024-03-28 00:10:41 +0000

    [ADD] Task 13: Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+SELECT tv_genre.name AS genre,
+COUNT(tv_genre.name) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_shows_genre
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name;
