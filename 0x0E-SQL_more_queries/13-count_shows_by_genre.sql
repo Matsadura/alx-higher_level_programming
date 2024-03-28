7e48ba95a86a8d3112bd4dfb41e19ed03fcc5898
Matsadura <zidanditoo1@gmail.com>
2024-03-28 00:14:29 +0000

    [UPDATE] Fix Typo

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_genre.name AS genre,
 COUNT(tv_genre.name) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_shows_genre
+LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
