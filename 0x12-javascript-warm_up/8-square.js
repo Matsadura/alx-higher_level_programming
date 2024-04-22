9af2a7a3a7914f6d66b46d5ed318c7001d0feadf
Matsadura <zidanditoo1@gmail.com>
2024-04-22 21:59:36 +0100

    [UPDATE] Remove one loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,17 +3,12 @@
 const { argv } = require('node:process');
 const size = Number.parseInt(argv[2]);
 
-let i, j;
+let i;
 
 if (isNaN(size)) {
   console.log('Missing Size');
 } else {
   for (i = 0; i < size; i++) {
-    let mySquare = '';
-
-    for (j = 0; j < size; j++) {
-      mySquare += 'X';
-    }
-    console.log(mySquare);
+    console.log('X'.repeat(size));
   }
 }
