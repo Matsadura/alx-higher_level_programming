4d1f2a7edb1a61c933f02aafef06f897d7bb9987
Matsadura <zidanditoo1@gmail.com>
2024-04-27 17:16:26 +0100

    [UPDATE] Use BINARY to perform Case sensitive comparison

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(
-            state_name)
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id;"\
+            .format(state_name)
     cur.execute(query)
 
     query_result = cur.fetchall()
