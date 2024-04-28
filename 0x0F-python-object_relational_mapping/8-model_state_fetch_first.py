15f5cc4b9636aaa0f2827f910b8d3164411d1e90
Matsadura <zidanditoo1@gmail.com>
2024-04-28 12:22:47 +0100

    [UPDATE] Add Condition to check if State is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,6 +20,9 @@ if __name__ == "__main__":
     session = Session()
 
     first_state = session.query(State).order_by(State.id).first()
-    print(f"{first_state.id}: {first_state.name}")
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
 
     session.close()
