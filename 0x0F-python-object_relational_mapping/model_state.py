3bf78679c1e3f3032c5475c672e000a1af3ccf15
Matsadura <zidanditoo1@gmail.com>
2024-04-30 23:34:35 +0100

    [UPDATE] Complete Relationship for DBStorage and FileStorage

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Contains the class State """
+from os import getenv
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,3 +16,15 @@ class State(Base):
                 nullable=False,
                 autoincrement=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                          backref="State",
+                          cascade="all, delete-orphan")
+
+    # Getter incase of storage type is FileStorage
+    if getenv('HBNB_TYPE_STORAGE') == db:
+        @property
+        def cities(self):
+            from models import storage
+            """ getter method that returls list of City instances """
+            return [City for Cities in storage.all("City").items()
+                    if City.state_id == self.id]
