f320480e9407aad0e128fc224b14c20a51bee1b4
Matsadura <zidanditoo1@gmail.com>
2024-05-09 22:36:42 +0100

    [UPDATE] Change host to ''

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the body of the response
-curl -s -I 0.0.0.0:5000 | grep 'Content-Length' | cut -d ' ' -f 2
+curl -s -I "$2" | grep 'Content-Length' | cut -d ' ' -f 2
