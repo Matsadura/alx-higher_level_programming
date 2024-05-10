0dd19b13e0f7becf129e29d1f89966ad345124fb
Matsadura <zidanditoo1@gmail.com>
2024-05-10 14:31:35 +0100

    [ADD] Task 8: Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL
+    and displays the body of the response."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {}
+    if len(argv) > 1:
+        data['q'] = argv[1]
+    else:
+        data['q'] = ""
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+    if type(eval(response.text)) is not dict:
+        print('Not a valid JSON')
+    elif len(eval(response.text)) == 0:
+        print('No result')
+    else:
+        id = eval(response.text).get('id')
+        name = eval(response.text).get('name')
+        print('[{}] {}'.format(id, name))
