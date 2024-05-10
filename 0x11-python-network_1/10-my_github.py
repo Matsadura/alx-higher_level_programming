d28cf97071e12b88ac840916f00ceb427a65af1d
Matsadura <zidanditoo1@gmail.com>
2024-05-10 19:12:26 +0100

    [ADD] Task 10: My GitHubgit add 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A script that takes your GitHub credentials (username and password)
+    and uses the GitHub API to display your id """
+from sys import argv
+import requests
+
+response = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+id = response.json().get('id')
+print(id)
