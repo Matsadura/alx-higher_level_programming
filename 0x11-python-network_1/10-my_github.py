089c50744e8c5b5308877d1cb53103b547d06a7a
Matsadura <zidanditoo1@gmail.com>
2024-05-10 19:22:55 +0100

    [UPDATE] Add if name

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ A script that takes your GitHub credentials (username and password)
     and uses the GitHub API to display your id """
-from sys import argv
 import requests
+from sys import argv
 from requests.auth import HTTPBasicAuth
 
-auth = HTTPBasicAuth(argv[1], argv[2])
-response = requests.get('https://api.github.com/user', auth=auth)
-id = response.json().get('id')
-print(id)
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    response = requests.get('https://api.github.com/user', auth=auth)
+    id = response.json().get('id')
+    print(id)
