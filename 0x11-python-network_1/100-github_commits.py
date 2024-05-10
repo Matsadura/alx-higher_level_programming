ad60ae8660ad932ea65b87de68ba3841da212a19
Matsadura <zidanditoo1@gmail.com>
2024-05-10 19:33:25 +0100

    [ADD] Task 10: Time for an interviewgit add 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Lists 10 commits from most recent to oldest """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    response = requests.get(url)
+    response = response.json()
+    for i in range(10):
+        sha = response[i]['sha']
+        author = response[i]['commit']['author']['name']
+        print("{}: {}".format(sha, author))
