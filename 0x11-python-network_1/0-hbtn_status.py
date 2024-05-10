f766500897e851a26951feffc0c0becfae7a2df5
Matsadura <zidanditoo1@gmail.com>
2024-05-10 10:27:39 +0100

    [ADD] What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A script that fetches intranet.com/status """
+from urllib.request import urlopen, Request
+
+if __name__ == "__main__":
+    req = Request('https://alx-intranet.hbtn.io/status')
+    with urlopen(req) as res:
+        body = res.read()
+
+    print(f"Body response:\n",
+          f"\t- type: {body.__class__}\n",
+          f"\t- content: {body}\n",
+          f"\t- utf8 content: {body.decode()}")
