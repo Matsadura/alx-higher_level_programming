ee81d76ad1df1129993ea414a53dabd3d239f52f
Matsadura <zidanditoo1@gmail.com>
2024-06-04 15:07:45 +0100

    [UPDATE] Refactor

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,15 @@
 
 const argv = process.argv;
 const request = require('request');
-const url = argv[2];
-const charId = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+let myApi = argv[2].split('/');
+myApi = myApi[4];
 
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const bodyJson = JSON.parse(body);
-    let i = 0;
-    for (const result of bodyJson.results) {
-      if (result.characters.includes(charId)) {
-        i++;
-      }
-    }
-    console.log(i);
+    console.log(bodyJson[myApi].length);
   }
 });
