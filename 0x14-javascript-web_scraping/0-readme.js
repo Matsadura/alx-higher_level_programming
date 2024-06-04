14dfeaac9885b470121f71079b182f5c864903b3
Matsadura <zidanditoo1@gmail.com>
2024-06-04 11:47:13 +0100

    [UPDATE] Add UTF-8 & Delete .toString()

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const argv = process.argv;
 const fs = require('fs');
 
-fs.readFile(argv[2], function (err, data) {
+fs.readFile(argv[2], 'UTF-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data.toString());
+    console.log(data);
   }
 });
